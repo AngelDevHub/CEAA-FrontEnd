478996b7a1e03cd282b941bfde99db64ef5edb26
AngelDevHub <rvangel44gc@gmail.com>
2025-11-13 13:08:37 -0600

    Cambios

--- package/src/services/AuthService.js
+++ package/src/services/AuthService.js
@@ -84,7 +84,7 @@ export function getCurrentUser() {
  */
 export async function refreshAccessToken(dispatch) {
     try {
-        const response = await axiosInstance.get('auth/refrescar', { withCredentials: true });
+        const response = await axiosInstance.get('auth/refresh-token', { withCredentials: true });
         if (response.data.success) {
             // Actualizar userDetails en localStorage si viene info nueva
             const current = getCurrentUser();
