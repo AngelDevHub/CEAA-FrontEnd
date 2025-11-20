cd61496869defc95e778d9f75fcb6a1d419a8001
AngelDevHub <rvangel44gc@gmail.com>
2025-11-19 20:17:24 -0600

    Login

--- package/vite.config.js
+++ package/vite.config.js
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default defineConfig({  
+export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1200,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['apexcharts', 'react-apexcharts', 'react-chartjs-2', 'chart.js', 'react-sparklines'],
+          ui: ['react-bootstrap', 'mdb-react-ui-kit', 'react-select', 'react-datepicker'],
+          editor: ['@ckeditor/ckeditor5-build-classic', '@ckeditor/ckeditor5-react'],
+          calendar: ['@fullcalendar/daygrid', '@fullcalendar/interaction', '@fullcalendar/react', '@fullcalendar/timegrid']
+        }
+      }
+    }
+  }
 })
